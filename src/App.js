2bb476c620724d7183efe56369b7ba97eac839dd
Fuad Ragib <ragibfuad@gmail.com>
2022-10-06 13:56:14 +0600

    first lecture of module 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Card from "./components/Card";
 import Child from "./components/Child";
+import CTA from "./components/CTA";
 import RoundedImg from "./components/RoundedImg";
 
 function App() {
     return (
         <div>
+            <CTA>
+                <h1>This is an important CTA</h1>
+                <button>Click me now or you will miss out!</button>
+            </CTA>
+            <CTA>
+                <form>
+                    <input type="text" />
+                    <br />
+                    <button>Submit</button>
+                </form>
+            </CTA>
             <RoundedImg src="https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/79877/optimized_large_thumb_stage.jpg" />
             <Child />
             <Card cardColor="red" height={200} width={200}></Card>
